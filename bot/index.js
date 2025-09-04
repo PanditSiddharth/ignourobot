d1a75a086931565094eaf04811152a1698d1f50b
Siddharth sharma <mynmss03@gmail.com>
2025-09-04 20:53:05 +0530

    test44

--- bot/index.js
+++ bot/index.js
@@ -1,19 +1,19 @@
 // import { insertUserData, insertGroupData } from "./handlers.js";
 // import { findUserById, getBotStats } from "./statsFacade.js";
 // import { notifyPrivateUsers } from "./notify.js";
-import { configDotenv } from "dotenv";
+// import { configDotenv } from "dotenv";
 // import { start } from "./botInfo.js";
-import connect from "../main/connect.js";
+// import connect from "../main/connect.js";
 
 // import { getMarks, getSeasonButtons } from "../handlers/gradeAndMarks.js";
 // import { statusHandler } from "../handlers/getFormattedResult.js";
 // import { fetchResult } from "../api/fetchResult.js";
 
 
-configDotenv();
+// configDotenv();
 export const runBot = async (bot) => {
 // DB connect
-await connect(process.env.MONGODB_URI);
+// await connect(process.env.MONGODB_URI);
 bot.use(async (ctx, next) => {
   try {
     console.log("Update handled");
